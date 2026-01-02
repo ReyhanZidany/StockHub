0b95c18e2561986cadd7f85f9ea4daa87eaeaea8
ReyhanZidany <reyzidan23@gmail.com>
2026-01-02 17:23:37 +0700

    feat: fix url

--- erp_frontend/src/api/axios.js
+++ erp_frontend/src/api/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { getAccessToken, clearTokens } from "../utils/auth";
 
 const api = axios.create({
-  baseURL: "http://localhost:3000/api/v1",
+  baseURL: "https://stockhub-production.up.railway.app/api/v1",
 });
 
 api.interceptors.request.use((config) => {
